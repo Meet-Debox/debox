03af4f34a877af62b51791ebd6d5b9bad4ef60a7
Meet-Debox <108864764+Meet-Debox@users.noreply.github.com>
2022-10-31 15:43:39 +0530

    typo fix

--- components/Description.tsx
+++ components/Description.tsx
@@ -5,7 +5,7 @@ function Description() {
     <div className="max-w-5xl mx-auto text-center cont w-11/12 ">
       <p>
         {
-          "In a world full of uncertainty, the companies that have survived the test of time are the ones that were able to build a highly engaged team and drive them to achieve a common goal and deliver path-breaking results consistently. "
+          "In a world full of uncertainties, the companies that have survived the test of time are the ones that were able to build a highly engaged team and drive them to achieve a common goal and deliver path-breaking results consistently. "
         }
       </p>
 
